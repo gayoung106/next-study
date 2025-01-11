89d8ed1a51e8c2b2cf7d9d11efe192e1ea4cf818
gayoung106 <xjongamx@kyonggi.ac.kr>
2025-01-11 11:47:42 +0900

    fix: 클라이언트 컴포넌트에 children으로 서버컴포넌트 받기

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,13 +1,14 @@
-"use client";
-
 import ClientCompoent from "./client-component";
 import styles from "./page.module.css";
+import ServerComponent from "./server-component";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       인덱스 페이지
-      <ClientCompoent />
+      <ClientCompoent>
+        <ServerComponent />
+      </ClientCompoent>
     </div>
   );
 }
