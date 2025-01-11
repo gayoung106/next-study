f26b6bc945c19bda6534b545c8ca23cd94518ee3
gayoung106 <xjongamx@kyonggi.ac.kr>
2025-01-11 11:44:32 +0900

    feat: 서버컴포넌트, 클라이언트컴포넌트 하이드레이션 비교

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import ClientCompoent from "./client-component";
 import styles from "./page.module.css";
 
 export default function Home() {
-  console.log("서버컴포넌트");
-  return <div className={styles.page}>인덱스 페이지</div>;
+  return (
+    <div className={styles.page}>
+      인덱스 페이지
+      <ClientCompoent />
+    </div>
+  );
 }
